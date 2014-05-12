f6055af6619997261985bf13ad18f180bbc3bc24
Fred Souza <fredericoamsouza@gmail.com>
2014-05-12 20:00:49 -0300

    Atualizacao do layout do site para ficar parecido com o aplicativo;

--- js/app.js
+++ js/app.js
@@ -1,19 +1,46 @@
 var app = {
-    initialize: function() {
-		var mapDiv = document.getElementById('map-canvas');
-		map = new google.maps.Map(mapDiv, {
-			center: new google.maps.LatLng(-22.9083, -43.1964),
-			zoom: 11,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		});
-        this.bindEvents();
+    openModal: false,
+    initialize: function(){
+        /*if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(
+                app.geolocationSuccess,
+                app.geolocationError,
+                {timeout:5000}); // Tempo limite pra tentar carregar alguma coisa
+        }
+        else*/ app.createMap();
     },
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    geolocationSuccess: function(position){
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        userLocation = new google.maps.LatLng(lat, lon);
+        app.createMap();
+        app.showDevicePosition(userLocation);
     },
-    onDeviceReady: function(){
-    		
+    geolocationError: function(error){
+        console.log('[ERROR '+ error.code + '] ' + error.message);
+        toast("Não foi possível recuperar sua localização.", 3);
+        app.createMap();
     },
-    receivedEvent: function(id) {
+    createMap: function(){
+        var mapDiv = document.getElementById('map-canvas');
+
+        var location = (userLocation)? userLocation : new google.maps.LatLng(-22.9083, -43.1964);
+        map = new google.maps.Map(mapDiv, {
+            center: location,
+            zoom: 12,
+            disableDefaultUI: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
+    },
+    showDevicePosition: function(location){
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: iconBase + 'man_maps.png',
+        });
     }
 };
+
+google.maps.event.addDomListener(window, 'load', app.initialize);
--- js/globals.js
+++ js/globals.js
@@ -1,19 +1,27 @@
 var map;
 var markers = [];
+var markersPositions = [];
+var bounds = new google.maps.LatLngBounds();
+var iconBase = "img/"; //'https://maps.google.com/mapfiles/kml/shapes/';
 var markerColors = ['red', 'yellow', 'green'];
+var loadTimeout = 0;
+var currentLine = '';
+var userLocation = false;
+var modalOpen = false;
 
-function addMarker (location, data) {  
+function addMarker(location, data) {
+    markersPositions.push(location);
     var dataBR = data[0].substring(3,6) + data[0].substring(0,2) + data[0].substring(5);
     var gpsTime = new Date(Date.parse(dataBR));
 
-	var iconUrl;
-	if (Math.abs(new Date() - gpsTime)/1000/60 > 10) {
-		iconUrl = "/img/bus_" + markerColors[0] + ".png";
-	} else if (Math.abs(new Date() - gpsTime)/1000/60 > 5) {
-		iconUrl = "/img/bus_" + markerColors[1] + ".png";
-	} else {
-		iconUrl = "/img/bus_" + markerColors[2] + ".png";
-	}
+    var iconUrl;
+    if (Math.abs(new Date() - gpsTime)/1000/60 > 10) {
+        iconUrl = iconBase+"bus_" + markerColors[0] + ".png";
+    } else if (Math.abs(new Date() - gpsTime)/1000/60 > 5) {
+        iconUrl = iconBase+"bus_" + markerColors[1] + ".png";
+    } else {
+        iconUrl = iconBase+"bus_" + markerColors[2] + ".png";
+    }
   
     var marker = new google.maps.Marker({
         position: location,
@@ -21,91 +29,132 @@ function addMarker (location, data) {
         title: data[1] + " (" + data[0] + ")",
         icon: new google.maps.MarkerImage(iconUrl)
     });
-	marker.info = new google.maps.InfoWindow({
-		content: '<div style="line-height:1.35;overflow:hidden;white-space:nowrap;">' +
+    marker.info = new google.maps.InfoWindow({
+        content: '<div style="line-height:1.35;overflow:hidden;white-space:nowrap;">' +
                  "Código: " + data[1] + "</br>" +
-				 "Hora: " + gpsTime.toLocaleString('pt-BR') + "</br>" +
-				 "Velocidade: " + data[5] + " Km/h</br>" +
+                 "Hora: " + gpsTime.toLocaleString('pt-BR') + "</br>" +
+                 "Velocidade: " + data[5] + " Km/h</br>" +
                  "</div>"
-	});
-	google.maps.event.addListener(marker, 'click', function() {
-		marker.info.open(map, marker);
-	});
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        marker.info.open(map, marker);
+    });
     markers.push(marker);
 }
 
+function addInfowindow(marker, line, date, velocity) {
+    var contentString = '<div class="infowindow">' +
+        '<div class="line"><b>Linha: </b>' + line + '</div>' +
+        '<div class="date"><b>Data/Hora: </b>' + date + '</div>' +
+        '<div class="velocity"><b>Velocidade: </b>' + velocity + 'Km/h</div>' +
+        '</div>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+    });
+}
+
+function toast(text, timeout){
+    console.log(text);
+    var t = $("#toast");
+    t.html(text);
+    t.fadeIn();
+    setTimeout(function() {
+        t.fadeOut();
+    }, timeout*1000);
+}
+
+function in_array(needle, group){
+    var ret = false;
+    for(g in group){
+        if(g==needle){
+            ret = true;
+            break;
+        }
+    }
+    return ret;
+}
+
 function setAllMap(map) {
-	for (var i = 0; i < markers.length; i++) {
-		markers[i].setMap(map);
-	}
+    for (var i = 0; i < markers.length; i++)
+        markers[i].setMap(map);
 }
 
 function clearMarkers() {
-	setAllMap(null);
+    setAllMap(null);
+}
+
+function clearMarkersPositions() {
+    markersPositions = [];
 }
 
 function showMarkers() {
-	setAllMap(map);
+    setAllMap(map);
 }
 
 function deleteMarkers() {
-	clearMarkers();
-	markers = [];
-}
-function initialize() {
-	var mapDiv = document.getElementById('map-canvas');
-	map = new google.maps.Map(mapDiv, {
-	center: new google.maps.LatLng(-22.9083, -43.1964),
-	zoom: 13,
-	mapTypeControl: true,
-	zoomControl: true,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-	});
-	trafficLayer = new google.maps.TrafficLayer();
-	trafficLayer.setMap(map);
+    clearMarkers();
+    markers = [];
+    markersPositions = [];
 }
 
 google.maps.Map.prototype.clearMarkers = function() {
-    for(var i=0; i < this.markers.length; i++){
-		this.markers[i].setMap(null);
-    }
+    for(var i=0; i < this.markers.length; i++)
+        this.markers[i].setMap(null);
     this.markers = new Array();
 };
 
-var loadTimeout = 0;
-var currentLine = '';
-function Load(){
-    var line = currentLine + "_" + loadTimeout;
-    $.getJSON("/proxy.php",
-        {
-			s: "1",
-            linha: currentLine,
-            rand: Math.round(Math.random()*999999)			
-        },
-        function(data) {
-            if (line != currentLine + "_" + loadTimeout) return;
-
-            setAllMap(null);
-            for (var i = 0; i < data.DATA.length; i++) {
-                var latLng = new google.maps.LatLng(data.DATA[i][3],
-                    data.DATA[i][4]);
-                addMarker(latLng, data.DATA[i]);
-            }
-			ga('send', 'pageview');
-    		clearTimeout(loadTimeout);
-            loadTimeout = setTimeout(function(){ Load(); }, 15000);
-        }
-    );
-}
-$(document).on("click","#search", function(event){
-    event.preventDefault();
+function findBus(clicked){
     currentLine = $("#busLine").val();
-    Load();
-});
+    var line = currentLine + "_" + loadTimeout;
+    $.getJSON("/proxy.php",{
+            s: "1", 
+            linha: currentLine,
+            rand: Math.round(Math.random()*999999)
+        },
+        function(data){
+            if (line != currentLine + "_" + loadTimeout) return;
+            $("#spinner").hide(0);
+            if(data.DATA.length==0)
+                toast("Desculpe, não encontrei esta linha.", 3);
+            else{
+                setAllMap(null);
+                clearMarkersPositions();
+                for (var i = 0; i < data.DATA.length; i++) {
+                    var latLng = new google.maps.LatLng(data.DATA[i][3], data.DATA[i][4]);
+                    addMarker(latLng, data.DATA[i]);
+                }
+                if(clicked){
+                    for (var i = 0, LtLgLen = markersPositions.length; i < LtLgLen; i++)
+                        bounds.extend(markersPositions[i]);
+                     map.fitBounds(bounds);
+                }
 
-$(document).on("click","#about", function(event){
+                ga('send', 'pageview');
+                clearTimeout(loadTimeout);
+                loadTimeout = setTimeout(function(){ findBus(false); }, 15000);
+                console.log("A busca retornou "+data.DATA.length+" resultados.");
+            }
+    }).error(function(e){
+        $("#spinner").hide(0);
+        toast("Desculpe, ocorreu algum erro. Tente novamente.", 3);
+        console.log(e.statusText);
+    });
+}
+
+$("#searchBox").submit(function(event){
     event.preventDefault();
-    $("#aboutText").bPopup();
+    $("#spinner").show(0);
+    $("#busLine").blur();
+    findBus(true);
 });
 
-google.maps.event.addDomListener(window, 'load', initialize);
+$('.modal').easyModal({top: 20});
+
+$('#btn-about').click(function(e){
+    e.preventDefault();
+    $('.modal').trigger('openModal');
+    app.openModal = true;
+});
