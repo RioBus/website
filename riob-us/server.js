05c394544849141074739756dc33869861b24067
Cassiohg <cassiohg@gmail.com>
2014-10-17 17:01:41 -0300

    pretending that main thread is doing stuff

--- riob-us/server.js
+++ riob-us/server.js
@@ -1,3 +1,9 @@
+setInterval(function (argument) {
+	console.log('Server doing stuff')
+},2000)
+
 var fork = require('child_process').fork,
-	child = fork("dataGraber.js");
+	child = fork(__dirname + "/dataGrabber.js");
+
+// module express could enter here
 
