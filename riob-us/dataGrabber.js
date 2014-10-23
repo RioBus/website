ecdcd528537df9e41fbaba50194addf04ec6c893
Cassiohg <cassiohg@gmail.com>
2014-10-23 14:58:57 -0200

    process now respond to parent messages
    - data is now used on thread process so i can use it on the
    process.send() method
    - checking for ECONNRESET error on response.
    - not saving json data on a json file. just keeping things on data
    object.

--- riob-us/dataGrabber.js
+++ riob-us/dataGrabber.js
@@ -25,109 +25,119 @@ var zlib = require('zlib'); // importing zlib module that we will use to decompr
 
 var time_endOfRequest, time_endOfResponseMoment, time_endResponseHeader;
 
+/*	object that is here to represent a simple data structure. this object will hold all the busses
+	from each bus line. the bus lines in this object will be sent to the server.js thread, whenever it receives
+	am http request for a bus line.
+*/
+var data = {};
+
 // function that will be called when we receive a response from dadosabertos server
 var httpGETCallback = function (response) {
 	time_endResponseHeader = (new Date()).getTime();
 	console.log(" >>> Response header took " + (time_endResponseHeader - time_endOfRequest) 
 					+ " miliseconds to arrive")
 
-	console.log('STATUS: ' + response.statusCode); // printing http status code from the server's response 
-	console.log('HEADERS: ' + JSON.stringify(response.headers)); // printing http header from the server's response
-	// these two prints are not necessary, but this is the place to check for status codes that differ from '200'
+	console.log('STATUS: ' + response.statusCode); // printing http status code from the server's response
+	if (response.statusCode == 'ECONNRESET'){
+		console.log("server closed the connection");
+	} else {
+		console.log('HEADERS: ' + JSON.stringify(response.headers)); // printing http header from the server's response
+		// these two prints are not necessary, but this is the place to check for status codes that differ from '200'
 
-	var json = ''; // variable that will hold the json received from dadosabertos server
-	var chunksCounter = 0; // chunk counter, just to see how things work...
+		var json = ''; // variable that will hold the json received from dadosabertos server
+		var chunksCounter = 0; // chunk counter, just to see how things work...
 
-	/*	registering function that will be called if there is an error on response. When response triggers 
-		the 'data' event. I don't know which types of error it could be. 
-	*/
-	response.on('error', function(err) {
-       console.log("We've had this error: " + err);
-    });
-
-	/*	in here we need to check if the response we are getting is compressed in gzip. if it is, we have to
-		instantiate a gzip decompresser and pass all the data from response to this gzip decompresser.
-		in the end, we set the object that will be notified by the .on('data') and .on('end') events. both
-		the response and the gzip decompresser listen to these two events.
-	*/
-    var output; // the object that will listen for 'data' and 'end' events
-    if (response.headers['content-encoding'] == 'gzip') { // the server tell us which kind of thing it is sending
-      var gzip = zlib.createGunzip(); // creating the gzip decompresser
-      response.pipe(gzip); // sending data from the responses (compressed) to the decompresser
-      output = gzip; // the decompresser will listen for 'data' and 'end' events
-    } else {
-      output = response; //the response will listen for 'data' and 'end' events
-    }
-
-	/*	registering function that will be called at every chunk received by either the response
-		or the decompresser. When the 'data' event is triggered.
-	*/
-	output.on('data', function (chunk) {
-		json += chunk.toString('utf-8'); // appending all the chunks
-		chunksCounter++; // couting one more chunk to this response
-	});
-
-	/*	registering function that will be called when data is completely received.
-		When the 'end' event is triggered.
-	*/
-	output.on('end', function () {
-		console.log(" --- there were " + chunksCounter + " chunks in this response"); // printing number of chunks 
-		time_endOfResponseMoment = (new Date()).getTime(); // moment when the whole response is completely received
-		console.log("Server took " + (time_endOfResponseMoment - time_endOfRequest) 
-					+ " miliseconds to finish the response");
-
-		json = JSON.parse(json); // parsing all the data, read as a string, as JSON. now, it's a javascript object
-		console.log("There are " + json['DATA'].length + " buss' GPS's on-line")
-
-		var data = {}; // object that is here to represent a simple data structure
-		/*
-			data will be a hashtable/hashmap, where the key will be the bus line and the value
-			will be all the busses on this line that came in the JSON response, like this:
-
-			key 			: 	value 
-			"<bus line>"	: 	[<bus info>, <bus info>, ...]
-
-			where <bus info> = ["DATAHORA","ORDEM","LINHA","LATITUDE","LONGITUDE","VELOCIDADE","DIRECAO"]
-			and <bus line> = "LINHA"
-
-			I have decided to build the structure in this way because I believe this is the way we should build
-			our future database. This structre makes the search for all the busses in a bus line, retrieve a
-			single value from one key. This is the main operation done in the project: a search for all the busses
-			from one bus line.
+		/*	registering function that will be called if there is an error on response. When response triggers 
+			the 'data' event. I don't know which types of error it could be. 
 		*/
+		response.on('error', function(err) {
+		   console.log("We've had this error: " + err);
+		});
 
-		// loop running backwards, according to v8's engine recommendation
-		for (var i = json['DATA'].length - 1; i >= 0; i--) {
-			var key = "" + json['DATA'][i][2]; // string that will be the key for the hash structure. 
-			// "" + INTEGER, parses the INTEGER to a string. javascript's fastest way parse from integer to string.
-			if (data[key]){ // if key already exists in data structure
-				data[key].push(json['DATA'][i]); // add this bus to this key (add bus to its respective line)
-			} else { // if key doesn't exist
-				data[key] = []; // instantiate an array in the key
-				data[key].push(json['DATA'][i]); // add this bus to this key (add bus to its respective line)
-			}
+		/*	in here we need to check if the response we are getting is compressed in gzip. if it is, we have to
+			instantiate a gzip decompresser and pass all the data from response to this gzip decompresser.
+			in the end, we set the object that will be notified by the .on('data') and .on('end') events. both
+			the response and the gzip decompresser listen to these two events.
+		*/
+		var output; // the object that will listen for 'data' and 'end' events
+		if (response.headers['content-encoding'] == 'gzip') { // the server tell us which kind of thing it is sending
+		  var gzip = zlib.createGunzip(); // creating the gzip decompresser
+		  response.pipe(gzip); // sending data from the responses (compressed) to the decompresser
+		  output = gzip; // the decompresser will listen for 'data' and 'end' events
+		} else {
+		  output = response; //the response will listen for 'data' and 'end' events
 		}
 
-		/*	this is the part where we should store the data in a database.
-			by now, we just print some shit about the response and write a json file with the data organized
-			by bus line.
+		/*	registering function that will be called at every chunk received by either the response
+			or the decompresser. When the 'data' event is triggered.
 		*/
-		var keys = Object.keys(data); // return all the keys in our simple data structure
-		// console.log(keys); // print all keys
-		console.log(" --- Number of bus lines = " + keys.length); // print the amount of keys
-
-		/*
-			writing a JSON file containing everything that is inside our data.
-			- JSON.stringify(data) turns the object into string as JSON format.
-			- JSON.stringify(data, null, 4) writes a JSON string with new lines 
-			after commas (",") and with a paragraph size of 4 spaces
-		*/
-		fs.writeFile('dataGrabbed.json', JSON.stringify(data), function (err) {
-			if (err) 
-				throw err;
-			console.log('It\'s saved!');
+		output.on('data', function (chunk) {
+			json += chunk.toString('utf-8'); // appending all the chunks
+			chunksCounter++; // couting one more chunk to this response
 		});
-	});
+
+		/*	registering function that will be called when data is completely received.
+			When the 'end' event is triggered.
+		*/
+		output.on('end', function () {
+			console.log(" --- there were " + chunksCounter + " chunks in this response"); // printing number of chunks 
+			time_endOfResponseMoment = (new Date()).getTime(); // moment when the whole response is completely received
+			console.log("Server took " + (time_endOfResponseMoment - time_endOfRequest) 
+						+ " miliseconds to finish the response");
+
+			json = JSON.parse(json); // parsing all the data, read as a string, as JSON. now, it's a javascript object
+			console.log("There are " + json['DATA'].length + " bus GPS's on-line")
+
+			data = {};
+			/*
+				data will be a hashtable/hashmap, where the key will be the bus line and the value
+				will be all the busses on this line that came in the JSON response, like this:
+
+				key 			: 	value 
+				"<bus line>"	: 	[<bus info>, <bus info>, ...]
+
+				where <bus info> = ["DATAHORA","ORDEM","LINHA","LATITUDE","LONGITUDE","VELOCIDADE","DIRECAO"]
+				and <bus line> = "LINHA"
+
+				I have decided to build the structure in this way because I believe this is the way we should build
+				our future database. This structre makes the search for all the busses in a bus line, retrieve a
+				single value from one key. This is the main operation done in the project: a search for all the busses
+				from one bus line.
+			*/
+
+			// loop running backwards, according to v8's engine recommendation
+			for (var i = json['DATA'].length - 1; i >= 0; i--) {
+				var key = "" + json['DATA'][i][2]; // string that will be the key for the hashmap structure. 
+				// "" + INTEGER, parses the INTEGER to a string. javascript's fastest way parse from integer to string.
+				if (data[key]){ // if key already exists in data structure
+					data[key].push(json['DATA'][i]); // add this bus to this key (add bus to its respective line)
+				} else { // if key doesn't exist
+					data[key] = []; // instantiate an array in the key
+					data[key].push(json['DATA'][i]); // add this bus to this key (add bus to its respective line)
+				}
+			}
+
+			/*	this is the part where we should store the data in a database.
+				by now, we just print some shit about the response and write a json file with the data organized
+				by bus line.
+			*/
+			var keys = Object.keys(data); // return all the keys in our simple data structure
+			// console.log(keys); // print all keys
+			console.log(" --- Number of bus lines = " + keys.length); // print the amount of keys
+
+			/*
+				writing a JSON file containing everything that is inside our data.
+				- JSON.stringify(data) turns the object into string as JSON format.
+				- JSON.stringify(data, null, 4) writes a JSON string with new lines 
+				after commas (",") and with a paragraph size of 4 spaces
+			*/
+			// fs.writeFile('dataGrabbed.json', JSON.stringify(data), function (err) {
+			// 	if (err) 
+			// 		throw err;
+			// 	console.log('It\'s saved!');
+			// });
+		});
+	}
 }
 
 var intervalTime = 15000; // default intervalTime to be passed as argument in the setInterval function later on
@@ -159,8 +169,10 @@ var sendRequestAndWriteResponse = function() {
 		instead of http.request()
 	*/
 	var get = http.get(options, httpGETCallback); //sending a request
-	console.log("Request Sent")
-	time_endOfRequest = (new Date()).getTime();
+	time_endOfRequest = new Date();
+	console.log("Request Sent at " + time_endOfRequest.getHours() + ":" + time_endOfRequest.getMinutes()
+				+ " and " + time_endOfRequest.getSeconds() + " seconds");
+	time_endOfRequest = time_endOfRequest.getTime();
 
 
 	// registering function that will be called if our request trigger the 'error' event
@@ -181,3 +193,14 @@ sendRequestAndWriteResponse(); // sending the request
  	// repeating the request every 15 seconds
  	sendRequestAndWriteResponse();	
  }, intervalTime); //intervalTime comes from the JSON configuration file
+
+
+/*	function that will be executed when this thread receives a message from its parent thread.
+*/
+process.on('message', function (message) {
+// it will send back to its parent, an array containing all the busses on the bus line specified in the parents message.
+	var busLine = message.busLine;
+	if (typeof data[busLine] !== 'undefined')
+		process.send(data[busLine].length); 
+	// it's expected the attribute 'busLine', as a string, inside the message object
+});
