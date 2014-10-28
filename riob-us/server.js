29c240fe2f746a1a9ae0b733c162e70e64e2fe57
Cassiohg <cassiohg@gmail.com>
2014-10-28 15:10:59 -0200

    sending json to user
    - user can now query a bus line and our server will respond with a json
    containing that information
    - using a fake datGrabber file to be used when dataGrabber can not
    fetch data from dadosabertos server (for the times when it is down)

--- riob-us/server.js
+++ riob-us/server.js
@@ -1,18 +1,64 @@
-var fork = require('child_process').fork,
-	child = fork(__dirname + "/dataGrabber.js");
-
-/*	function that will be executed when this thread receives a message from its child thread.
+/*	things to do in this code
+	1- keep updating comments as code changes.
+This way, we don't need to restart the code if those things change.
+	2- delete comments that print information that is useless for this code. bus line being queryed.
+This kind of information is useful for someone else, somewhere else, but not here, not for this code.
+	3- add the code that will serve the website pages
 */
+
+
+/* ==================
+	this is the server code. where we are going to serve a rest interface and the website pages.
+*/
+
+// we start by calling the dataGrabber.js file in another thread.
+// var fork = require('child_process').fork, // child processes are different threads that are simply new node threads.
+// 	child = fork(__dirname + "/dataGrabber.js");
+
+/* I am using this fakeDataGrabber as a temporary dataGrabber impersonation for the times when dadosabertos server
+	is down...
+*/
+var fork = require('child_process').fork,
+	child = fork(__dirname + "/fakeDataGrabber.js");
+
+// function that will be executed when this thread receives a message from its child thread.
+// child.on('message', function (message) {
+// 	console.log(message);
+// });
+
+
+var express = require('express'); // we are using express as our middleware. it has lots of cool functionalities.
+var url = require('url'); // we use url module to parse the url in the request, sent to us, and extract the bus line.
+
+var app = express(); // initializing a new express object (as if javascript were object oriented).
+
+var data = {}; // data will hold all the bus lines collected by the dataGrabber.js thread.
+// dataGrabber will send everything collected to this server.js thread.
 child.on('message', function (message) {
-	console.log(message);
-});
+	data = message.data; // message is the object passed from the child process
+})
 
-setInterval(function () {
-	child.send({busLine: "485"}); // sending a message to child process, asking for all busses in this bus line
-	// child process is expecting the field 'busLine', as a string, as an attribute of the message object
-},3000)
+//our first rout
+app.get('/', function (req, res) {
+	var line = Object.keys(url.parse(req.url, true).query)[0]; // getting the first string from the url request
+	console.log("-> user searching for line: " + line);
+	if (typeof line === 'string' && line != "") { // if it is a not empty string, we can send stuff from our data
+		// seding sutff from our data, using the same form as dadosabertos server sends their json.
+		res.json({COLUMNS:["DATAHORA","ORDEM","LINHA","LATITUDE","LONGITUDE","VELOCIDADE"], 
+					DATA: data[line]}); // our data enters here.
+	} else { // until now, theres nothing left to do.
+		res.send('hello world!'); 
+	}
+})
 
 
+// starting our server, using our express instance, on port 8080
+var server = app.listen(8080, function () {
 
+	var host = server.address().address;
+	var port = server.address().port;
 
+	console.log('Server listening at http://%s:%s', host, port);
+
+})
 
