901fbe3648076b14733f39c11a14f274c02d61d8
Cassiohg <cassiohg@gmail.com>
2014-11-20 11:45:00 -0200

    added guards for log messages
    log messages are not being written repeatedly. a log is only written if
    the last log was different.

--- riob-us/dataGrabber.js
+++ riob-us/dataGrabber.js
@@ -19,16 +19,16 @@ var winston = require('winston'); // importing library that will help us write b
 // function that returns our standart time stamp format. I'm using it for the loggers and for the json's 'lastUpdate'.
 function timeStamp () {return (new Date()).toLocaleString()};
 
-/*	creating a custom log writer. It will log on console, in a file for erros and warnings, and in another file for info's. 
+/*	creating a custom log writer. It will log on console and in a file. 
 	it seems that handling exceptions mean that it will log it and won't abort the code. im not sure. */
 var consoleTransportOptions = {
-	colorize: true,
+	colorize: true, // color is only visible on command line tool.
 	timestamp: timeStamp
 };
 var fileTransportOptions = { 
 	filename: 'dataGrabberInfoLog.log',
 	handleExceptions: true,
-	colorize: true,
+	colorize: true, // color is only visible on command line tool.
 	timestamp: timeStamp,
 	maxsize: 1*1024*1024, // size in bytes.
 	maxFiles: 2
@@ -42,7 +42,7 @@ var http = require('http'); // importing http module. it's a node's default modu
 var fs = require('fs');	// importing filesystem module. using fs to read riobus-config.json.
 var zlib = require('zlib'); // importing zlib module that we will use to decompress the JSON compressed in gzip.
 
-var lastTimeWasBad = false; // variable is set to true when dadosabertos server can't respond with http status code 200.
+var lastStatus = 0; // variable is set to status code responded by dadosabertos in its last data.
 
 // function that will be called when we receive a response from dadosabertos server.
 var httpGETCallback = function (response) {
@@ -52,22 +52,17 @@ var httpGETCallback = function (response) {
 	httpGetIntervalCode = setTimeout(sendRequestAndGrabData, intervalTime);
 
 	if (response.statusCode == 200) { // we can only do stuff if we receive an statusCode of 200.
-		// printing http header from the server's response
-		// logger.info(' - HEADERS: ' + JSON.stringify(response.headers));
+		// logger.info(' - HEADERS: ' + JSON.stringify(response.headers)); // printing http header from server's response.
 
-		logger.info('Dados recebidos');
-
-		if (lastTimeWasBad) { // last response had an status code different from 200.
-			logger.info("Dadosabertos is fine now"); // logging notice that we are back on trail.
-			lastTimeWasBad = false; // this is reponse hat status code 200, so this one is fine.
-		}
-
-		var json = ''; // variable that will hold the json received from dadosabertos server
+		var json = ''; // variable that will hold the json received from dadosabertos server.
 
 		/*	registering function that will be called if there is an error on response. When response triggers 
-			the 'data' event. I don't know which types of error it could be. */
+			the 'data' event. I don't know which types of error there could be. */
 		response.on('error', function(err) {
-		   logger.warn("We've had this error on dadosabertos RESPONSE: " + err);
+			if (lastStatus !== 'response error') { // guarding log messages so they don't spam consecutive equals.
+				logger.warn("We've had this error on dadosabertos RESPONSE: " + err);
+				lastStatus = 'response error'; // if it stops here, we are in the generic response error case.
+			}
 		});
 
 		/*	in here we need to check if the response we are getting is compressed with gzip. if it is, we have to
@@ -75,32 +70,35 @@ var httpGETCallback = function (response) {
 			in the end, we set either object (response or gzip) that will be notified by the .on('data') 
 			and .on('end') events. both the response and the gzip decompresser (it seems...) listen to these two events. 
 			I don't actually understand this 'pipe' thing. */
-		var output; // the object that will listen for 'data' and 'end' events
-		if (response.headers['content-encoding'] == 'gzip') { // the server tell us which kind of thing it is sending
-		  var gzip = zlib.createGunzip(); // creating the gzip decompresser
-		  response.pipe(gzip); // sending data from the responses (compressed) to the decompresser
-		  output = gzip; // the decompresser will listen for 'data' and 'end' events
+		var output; // the object that will listen for 'data' and 'end' events.
+		if (response.headers['content-encoding'] == 'gzip') { // the server tell us which kind of thing it is sending.
+			var gzip = zlib.createGunzip(); // creating the gzip decompresser.
+			response.pipe(gzip); // sending data from the responses (compressed) to the decompresser.
+			output = gzip; // the decompresser will listen for 'data' and 'end' events.
 		} else {
-		  output = response; //the response will listen for 'data' and 'end' events
+			output = response; // the response will listen for 'data' and 'end' events.
 		}
 
 		/*	registering function that will be called at every chunk received by either the response
 			or the decompresser. When the 'data' event is triggered. */
 		output.on('data', function (chunk) {
-			json += chunk.toString('utf-8'); // appending all the chunks
+			json += chunk.toString('utf-8'); // appending all the chunks.
 		});
 
 		/*	registering function that will be called when data is completely received.
 			When the 'end' event is triggered. */
 		output.on('end', function () {
 			try {
-				// parsing all the data, read as a string, as JSON. now, it's a javascript object
+				// parsing all the data, read as a string, as JSON. now, it's a javascript object.
 				json = JSON.parse(json);
 			} catch (err) {
 				json = null; // if there was an error when parsing the json, it is invalid to our purpose.
 				if (err instanceof SyntaxError) {
-					logger.warn("We've had a syntax error while parsing json file from dadosabertos." +
-								" Data will be an empty object");
+					if (lastStatus !== 'bad json'){ // last response wasn't case 'bad json'.
+						logger.warn("We've had a syntax error while parsing json file from dadosabertos. " +
+								"Data will be an empty object");
+						lastStatus = 'bad json'; // it means 'status.Code' is 200 but JSON could not be parsed.
+					}
 				} else {
 					logger.error(err.message);
 					logger.error(err.stack);		
@@ -109,18 +107,26 @@ var httpGETCallback = function (response) {
 
 			/*	If we received a message saying that our request was good but nothing has been found, we
 				will not consider it as a valid response. because it means their service could not provide
-				any data, which they should. As we are quering for everything, something must be provided.
-			*/
-
+				any data, which they should. As we are quering for everything, something must be provided. */
 			// checking if dadosabertos server gave us just a message telling us that nothing were found.
 			// "COLUMNS" attribute change to an array with size 1 and its content is'MENSAGEM'.
 			if (json['COLUMNS'][0] === "MENSAGEM") {
 				// we don't care about the message. this is the only message in the whole service.
-				logger.warn("Dadosabertos said:", json['DATA'][0][0]) // message comes inside 'DATA', nested 2 times.
 				json = null; // it means this json is invalid to our purpose.
+				if (lastStatus !== 'only message'){ // last response wasn't case 'only message'.
+					logger.warn("Dadosabertos said: " + json['DATA'][0][0]) // message comes inside 'DATA', nested 2 times.
+					lastStatus = 'only message';
+					// it means 'status.Code' is 200, JSON was parsed, but there's only a message in the JSON.
+				}
 			}
 
 			if (json !== null) { // if json is a valid object, keep going.
+				if (lastStatus !== 'success') { // last response wasn't case 'success'.
+					// logging notice that we are back on trail.
+					logger.info("Dadosabertos is fine. We have just got some data, code: " + response.statusCode);
+					lastStatus = 'success'; // this case means everything went well.
+				}
+
 				/*	object 'data' is here to represent a simple data structure. this object will hold all the busses
 					in each bus line. All bus lines in this object will be sent to the server.js thread everytime this
 					code runs. */
@@ -188,7 +194,7 @@ var httpGETCallback = function (response) {
 				// }
 
 				// sending 'data', 'json', 'orders' and lastUpdate to parent thread.
-				process.send({data: data, json: json.DATA, orders: orders, lastUpdate: timeStamp ()});
+				process.send({data: data, json: json, orders: orders, lastUpdate: timeStamp(), lastStatus: lastStatus});
 				/*	lastUpdate informs when we received the last successful response. transforming date to a 
 					readable UTC time string. it looks like this: "Sun Nov 02 2014 16:26:12 GMT-0200 (BRST)"*/
 
@@ -214,18 +220,24 @@ var httpGETCallback = function (response) {
 			}
 		});
 	} else { // if response's statusCode wasn't 200, than it's bad new.
-		lastTimeWasBad = true;
-		if (response.statusCode == 'ECONNRESET') { // statusCode for when remote server close the connection on us.
-			logger.warn("Dadosabertos server closed the connection, code: " + response.statusCode);
-		} else if (response.statusCode == 503) { // statusCode for when remote server is unavailable.
-			logger.warn("Dadosabertos server was unavailable, code: " + response.statusCode);
-		} else if (response.statusCode == 404) { // statusCode for when remote server can't find request (not found).
-			logger.warn("Dadosabertos server could not find anything matching the url, code: " + response.statusCode);
-		} else if (response.statusCode == 302) { // statusCode for when we remove server indicates url redirection.
-			logger.warn("Dadosabertos wants us to redirect our request to a new url, code: " + response.statusCode);
+		/*	writing an specific log message for each status code returned by dadosabertos but only if it's not
+			the same as the one we got in the previous response. I don't want to log the same thing over and over. */
+		if (response.statusCode == 'ECONNRESET') {
+			if (lastStatus !== 'ECONNRESET') // last response wasn't case string 'ECONNRESET'.
+				logger.warn("Dadosabertos server closed the connection, code: " + response.statusCode);
+		} else if (response.statusCode == 503) {
+			if (lastStatus !== 503) // last response wasn't case number 503.
+				logger.warn("Dadosabertos server was unavailable, code: " + response.statusCode);
+		} else if (response.statusCode == 404) { // 404 not found.
+			if (lastStatus !== 404) // last response wasn't case number 404.
+				logger.warn("Dadosabertos server could not find anything matching the url, code: " + response.statusCode);
+		} else if (response.statusCode == 302) {
+			if (lastStatus !== 302) // last response wasn't case number 302.
+				logger.warn("Dadosabertos wants us to redirect our request to a new url, code: " + response.statusCode);
 		} else {
 			logger.warn("Dadosabertos responded with statuscode: " + response.statusCode);
 		}
+		lastStatus = response.statusCode; // setting an identification of what we got in the last response.
 	}
 }
 
@@ -246,7 +258,7 @@ function sendRequestAndGrabData() {
 	var options = {
 		host: config.host, // comes from JSON configuration file
 		path: config.path, // comes from JSON configuration file
-		agent: false, // agent keeps connection alive until response. we turn it off so there's no agent keeping it alive.
+		agent: false, // agent keeps connection alive until response. we turn it off so there's nothing keeping it alive.
 		headers: { // we want to get the data enconded with gzip, after lots of trial and error, this is the right order
 	  		"Accept-Encoding": "gzip", // we first say it has to be compacted with gzip
 			"Accept": "application/json" // then we say which format we want to receive
@@ -257,24 +269,30 @@ function sendRequestAndGrabData() {
 		http.get(options, [callback]) function makes a request using method GET and calls request.end() automatically.
 		I don't think we need to keep the connection alive and we don't need a body. that's why I decided for http.get()
 		instead of http.request() */
-	// SENDING REQUEST NOW. this is when things actually start. the rest of the code is still to be run.
+	// SENDING REQUEST RIGHT NOW. this is when things actually start. the rest of the code above is still to be run.
 	var requestGet = http.get(options, httpGETCallback);
 
 	/*	setting a callback function that runs when our request's times out. when the request times out, the connection 
 		is still up. It's nothing more than a simple javsascript 'setTimeout()' scheduler underneath this call. */
 	requestGet.setTimeout(config.timeout, function () {
-		logger.warn('Our REQUEST has timed out.');
+		if (lastStatus !== 'timeout') { // checking if we had a timeout in our last request.
+			logger.warn('Our REQUEST has timed out.'); // log message if we had.
+			lastStatus = 'timeout'; // setting an identification of what we got in the last response.
+		}
 		requestGet.abort(); // this makes the request emit an 'error' event. because it force closes the current connection.
 	})
 
 	// registering function that will be called if our request triggers an 'error' event.
-	requestGet.on('error', function (e) { 
-		lastTimeWasBad = true; // because errors are bad. we treat this variable in the next sucessful response.
-		logger.warn('Our REQUEST has had this error: ' + e.message); //printing error message.
-		httpGetIntervalCode = setTimeout(sendRequestAndGrabData, intervalTime); // on erros, resend request.
+	requestGet.on('error', function (e) {
+		// checking if we had an error or timeout in our last request.
+		if (lastStatus !== 'request error' && lastStatus !== 'timeout') {
+			logger.warn('Our REQUEST has had this error: ' + e.message); // logging error message.
+			lastStatus = 'request error'; // setting an identification of what we got in the last response.
+		}
+		httpGetIntervalCode = setTimeout(sendRequestAndGrabData, intervalTime); // on errors, resend request after interval.
 		// calling 'clearInterval(httpGetIntervalCode)' stops the scheduled function corresponding to 'httpGetIntervalCode'.
 	});
 
 }
 
-sendRequestAndGrabData(); // running the code that sends the request.
+sendRequestAndGrabData(); // calling the code that sends the request.
